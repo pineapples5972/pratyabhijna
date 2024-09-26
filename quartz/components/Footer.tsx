394f2e65a7fa59232ef4acb2dd35409d8f5797a2
Ｎａｇｋｕｍａｒ です <99494692+karpurgauram@users.noreply.github.com>
2024-09-26 21:46:56 +0530

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,8 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          {i18n(cfg.locale).components.footer.allrightsreserved}{" "}
+          <a href="https://t.me/karpurgauram/">karpurgauram </a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
